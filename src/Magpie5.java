e37c81cec001db9113726370635398c7f95dfd76
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-11-01 19:20:55 -0400

    Update Magpie5.java

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"You think so?",
+			"Yeah, they really are.",
+			"I agree.",
+			"Yeah it's a great party trick."
 	};
 	
 }
